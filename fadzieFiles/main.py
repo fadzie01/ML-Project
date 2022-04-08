d6582289e8ccd1b3d368a093ee6ec0d8e9f6cf96
fadzie01 <86614801+fadzie01@users.noreply.github.com>
2022-04-08 22:47:00 +0200

    Add files via upload

---
+++ fadzieFiles/main.py
@@ -0,0 +1,54 @@
+
+import io 
+
+import upload_video as v
+
+import split_video as sv
+
+import classify as sst
+
+import cv2 as cv
+
+import tempfile
+import streamlit as st
+
+
+
+uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
+
+v.upload_video(uploaded_file)
+
+classifications = sst.classifyImages()
+
+#function to search in frames
+def searchInFrames(object_):
+    
+    indeces = []
+    
+    if object_ in classifications:
+        st.write(" The object has been found")
+  
+    else:
+        st.write("The object is not in the frames!")
+        
+  #input is our varible to store our input      
+input = st.text_input("Enter object to search: ")
+
+if st.button('Search'): 
+    
+    frames =[]
+    
+    detected_paths = []
+    
+    searchInFrames(input)
+    
+    st.write("")
+
+
+
+
+
+
+
+
+
